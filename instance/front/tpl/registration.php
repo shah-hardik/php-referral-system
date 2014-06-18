99a248b9abb3986ff0ab1ebcc9bac86b8bc824b3
Raj Bose <raj.bose1989@gmail.com>
2014-06-18 11:22:17 +0530

    Configuration

--- instance/front/tpl/registration.php
+++ instance/front/tpl/registration.php
@@ -1,19 +1,268 @@
-<div style="" class="moduleActionBar">
-    <div>
-        <nav class="navbar navbar-default " role="navigation">
-
-            <div class="collapse navbar-collapse navbar-ex1-collapse ">
-                <ul class="nav navbar-nav">
-                    <li class="<?php print $activeMenuList ?>"><a href="<?php print _U ?>registration/list"><i class="glyphicon glyphicon-th-list"></i>&nbsp;List Registration</a></li>
-                    <li class="<?php print $activeMenuAdd ?>"><a href="<?php print _U ?>registration/add"><i class="glyphicon glyphicon-<?php print $addIcon ?>"></i>&nbsp;<?php print $addLabel ?></a></li>
-		      <li class="disabled"><a href="#"><i class="glyphicon glyphicon-trash"></i>&nbsp;Delete Registration(s)</a></li>
-
-                </ul>
-            </div><!-- /.navbar-collapse -->
-        </nav>
-
-    </div>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Referral </title>
+<meta name="generator" content="WYSIWYG Web Builder 8 - http://www.wysiwygwebbuilder.com">
+<style type="text/css">
+div#container
+{
+   width: 994px;
+   position: relative;
+   margin-top: 0px;
+   margin-left: auto;
+   margin-right: auto;
+   text-align: left;
+}
+body
+{
+   text-align: center;
+   margin: 0;
+   background-color: #FFFFFF;
+   background-image: url(<?php print _MEDIA_URL?>img/registration.jpg);
+   background-repeat: no-repeat;
+   background-position: center top;
+   color: #000000;
+}
+</style>
+<link rel="stylesheet" href="<?php print _MEDIA_URL?>css/HP_referral.css" type="text/css">
+<style type="text/css">
+#wb_Form1
+{
+   background-color: #FFFFFF;
+   border: 0px #000000 solid;
+}
+#wb_Text1 
+{
+   background-color: transparent;
+   border: 0px #000000 solid;
+   padding: 0;
+}
+#wb_Text1 div
+{
+   text-align: left;
+   white-space: nowrap;
+}
+#Combobox1
+{
+   border: 1px #C0C0C0 solid;
+   background-color: #FFFFFF;
+   color: #000000;
+   font-family: Arial;
+   font-size: 13px;
+}
+#Editbox1
+{
+   border: 1px #C0C0C0 solid;
+   background-color: #FFFFFF;
+   color :#000000;
+   font-family: Arial;
+   font-size: 13px;
+   text-align: left;
+   vertical-align: middle;
+}
+#Editbox2
+{
+   border: 1px #C0C0C0 solid;
+   background-color: #FFFFFF;
+   color :#000000;
+   font-family: Arial;
+   font-size: 13px;
+   text-align: left;
+   vertical-align: middle;
+}
+#Editbox3
+{
+   border: 1px #C0C0C0 solid;
+   background-color: #FFFFFF;
+   color :#000000;
+   font-family: Arial;
+   font-size: 13px;
+   text-align: left;
+   vertical-align: middle;
+}
+#Editbox4
+{
+   border: 1px #C0C0C0 solid;
+   background-color: #FFFFFF;
+   color :#000000;
+   font-family: Arial;
+   font-size: 13px;
+   text-align: left;
+   vertical-align: middle;
+}
+#Editbox5
+{
+   border: 1px #C0C0C0 solid;
+   background-color: #FFFFFF;
+   color :#000000;
+   font-family: Arial;
+   font-size: 13px;
+   text-align: left;
+   vertical-align: middle;
+}
+#Editbox6
+{
+   border: 1px #C0C0C0 solid;
+   background-color: #FFFFFF;
+   color :#000000;
+   font-family: Arial;
+   font-size: 13px;
+   text-align: left;
+   vertical-align: middle;
+}
+#Editbox7
+{
+   border: 1px #C0C0C0 solid;
+   background-color: #FFFFFF;
+   color :#000000;
+   font-family: Arial;
+   font-size: 13px;
+   text-align: left;
+   vertical-align: middle;
+}
+#Editbox8
+{
+   border: 1px #C0C0C0 solid;
+   background-color: #FFFFFF;
+   color :#000000;
+   font-family: Arial;
+   font-size: 13px;
+   text-align: left;
+   vertical-align: middle;
+}
+#Editbox9
+{
+   border: 1px #C0C0C0 solid;
+   background-color: #FFFFFF;
+   color :#000000;
+   font-family: Arial;
+   font-size: 13px;
+   text-align: left;
+   vertical-align: middle;
+}
+#TextArea1
+{
+   border: 1px #C0C0C0 solid;
+   background-color: #FFFFFF;
+   color :#000000;
+   font-family: Arial;
+   font-size: 13px;
+   text-align: left;
+}
+#TextArea2
+{
+   border: 1px #C0C0C0 solid;
+   background-color: #FFFFFF;
+   color :#000000;
+   font-family: Arial;
+   font-size: 13px;
+   text-align: left;
+}
+#Editbox10
+{
+   border: 1px #C0C0C0 solid;
+   background-color: #FFFFFF;
+   color :#000000;
+   font-family: Arial;
+   font-size: 13px;
+   text-align: left;
+   vertical-align: middle;
+}
+#Editbox11
+{
+   border: 1px #C0C0C0 solid;
+   background-color: #FFFFFF;
+   color :#000000;
+   font-family: Arial;
+   font-size: 13px;
+   text-align: left;
+   vertical-align: middle;
+}
+#Editbox12
+{
+   border: 1px #C0C0C0 solid;
+   background-color: #FFFFFF;
+   color :#000000;
+   font-family: Arial;
+   font-size: 13px;
+   text-align: left;
+   vertical-align: middle;
+}
+#wb_Text2 
+{
+   background-color: transparent;
+   border: 0px #000000 solid;
+   padding: 0;
+}
+#wb_Text2 div
+{
+   text-align: left;
+   white-space: nowrap;
+}
+#wb_Text3 
+{
+   background-color: transparent;
+   border: 0px #000000 solid;
+   padding: 0;
+}
+#wb_Text3 div
+{
+   text-align: left;
+}
+</style>
+</head>
+<body>
+<div id="container">
+<div id="wb_Form1" style="position:absolute;left:24px;top:214px;width:937px;height:920px;z-index:20;">
+<form name="Form1" method="post" action="mailto:yourname@yourdomain.com" enctype="text/plain" id="Form1">
+<div id="wb_Text1" style="position:absolute;left:40px;top:179px;width:407px;height:504px;z-index:0;text-align:left;">
+<div style="line-height:36px;"><span style="color:#696969;font-family:Arial;font-size:16px;">Salutation:&nbsp; (drop down)</span></div>
+<div style="line-height:36px;"><span style="color:#696969;font-family:Arial;font-size:16px;">First Name:* 			</span></div>
+<div style="line-height:36px;"><span style="color:#696969;font-family:Arial;font-size:16px;">Last Name:* 			</span></div>
+<div style="line-height:36px;"><span style="color:#696969;font-family:Arial;font-size:16px;">Company/Brokerage:*&nbsp; 			</span></div>
+<div style="line-height:36px;"><span style="color:#696969;font-family:Arial;font-size:16px;">Street Address:* 			</span></div>
+<div style="line-height:36px;"><span style="color:#696969;font-family:Arial;font-size:16px;">City:* 			</span></div>
+<div style="line-height:36px;"><span style="color:#696969;font-family:Arial;font-size:16px;">Province:* 			</span></div>
+<div style="line-height:36px;"><span style="color:#696969;font-family:Arial;font-size:16px;">Postal Code:* 			</span></div>
+<div style="line-height:36px;"><span style="color:#696969;font-family:Arial;font-size:16px;">Primary Phone:* 			&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; </span></div>
+<div style="line-height:36px;"><span style="color:#696969;font-family:Arial;font-size:16px;">Email Address:*&nbsp; 			</span></div>
+<div style="line-height:36px;"><span style="color:#696969;font-family:Arial;font-size:16px;">How did you hear about our program?: </span></div>
+<div style="line-height:36px;"><span style="color:#696969;font-family:Arial;font-size:16px;"><br></span></div>
+<div style="line-height:36px;"><span style="color:#696969;font-family:Arial;font-size:16px;">						</span></div>
+<div style="line-height:18px;"><span style="color:#696969;font-family:Arial;font-size:16px;">Please enter any comments or questions you may have:</span></div>
 </div>
-<?php include_once('message.php') ?>
-
-<?php include $subTpl; ?>
+<select name="Combobox1" size="1" id="Combobox1" style="position:absolute;left:256px;top:174px;width:162px;height:26px;z-index:1;">
+<option selected value="Select One">Select One</option>
+<option value="Mr">Mr</option>
+<option value="Ms">Ms</option>
+</select>
+<input type="text" id="Editbox1" style="position:absolute;left:255px;top:208px;width:235px;height:24px;line-height:24px;z-index:2;" name="Editbox1" value="">
+<input type="text" id="Editbox2" style="position:absolute;left:255px;top:244px;width:235px;height:24px;line-height:24px;z-index:3;" name="Editbox1" value="">
+<input type="text" id="Editbox3" style="position:absolute;left:255px;top:316px;width:375px;height:24px;line-height:24px;z-index:4;" name="Editbox1" value="">
+<input type="text" id="Editbox4" style="position:absolute;left:255px;top:280px;width:277px;height:24px;line-height:24px;z-index:5;" name="Editbox1" value="">
+<input type="text" id="Editbox5" style="position:absolute;left:255px;top:388px;width:277px;height:24px;line-height:24px;z-index:6;" name="Editbox1" value="">
+<input type="text" id="Editbox6" style="position:absolute;left:255px;top:352px;width:277px;height:24px;line-height:24px;z-index:7;" name="Editbox1" value="">
+<input type="text" id="Editbox7" style="position:absolute;left:255px;top:460px;width:277px;height:24px;line-height:24px;z-index:8;" name="Editbox1" value="">
+<input type="text" id="Editbox8" style="position:absolute;left:255px;top:424px;width:277px;height:24px;line-height:24px;z-index:9;" name="Editbox1" value="">
+<input type="text" id="Editbox9" style="position:absolute;left:255px;top:493px;width:277px;height:24px;line-height:24px;z-index:10;" name="Editbox1" value="">
+<textarea name="TextArea1" id="TextArea1" style="position:absolute;left:41px;top:570px;width:495px;height:58px;z-index:11;" rows="2" cols="76"></textarea>
+<textarea name="TextArea1" id="TextArea2" style="position:absolute;left:42px;top:678px;width:495px;height:58px;z-index:12;" rows="2" cols="76"></textarea>
+<input type="text" id="Editbox10" style="position:absolute;left:254px;top:75px;width:235px;height:24px;line-height:24px;z-index:13;" name="Editbox1" value="">
+<input type="text" id="Editbox11" style="position:absolute;left:254px;top:36px;width:235px;height:24px;line-height:24px;z-index:14;" name="Editbox1" value="">
+<input type="text" id="Editbox12" style="position:absolute;left:254px;top:113px;width:235px;height:24px;line-height:24px;z-index:15;" name="Editbox1" value="">
+<div id="wb_Text2" style="position:absolute;left:38px;top:43px;width:407px;height:108px;z-index:16;text-align:left;">
+<div style="line-height:36px;"><span style="color:#696969;font-family:Arial;font-size:16px;">Username</span></div>
+<div style="line-height:36px;"><span style="color:#696969;font-family:Arial;font-size:16px;">Password</span></div>
+<div style="line-height:18px;"><span style="color:#696969;font-family:Arial;font-size:16px;">Confirm Password</span></div>
+</div>
+<div id="wb_Shape4" style="position:absolute;left:45px;top:840px;width:223px;height:54px;z-index:17;">
+<img src="images/img0005.gif" id="Shape4" alt="" style="border-width:0;width:223px;height:54px;"></div>
+<input type="checkbox" id="Checkbox1" name="" value="on" style="position:absolute;left:44px;top:776px;z-index:18;">
+<div id="wb_Text3" style="position:absolute;left:69px;top:778px;width:602px;height:16px;z-index:19;text-align:left;">
+<span style="color:#696969;font-family:Arial;font-size:13px;">Yes, I would like to receive information and updates.</span></div>
+</form>
+</div>
+</div>
+</body>
+</html>
