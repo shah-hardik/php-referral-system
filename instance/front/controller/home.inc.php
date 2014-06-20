3ff9b98f73820bb5e273f2ffae6db7faa7e9f055
shah-hardik <hardiks059@gmail.com>
2014-06-20 11:23:33 +0530

    login

--- instance/front/controller/home.inc.php
+++ instance/front/controller/home.inc.php
@@ -10,43 +10,39 @@
  * @package BePure
  * 
  */
-
-$data = q("select * from registration where id='6' ");
-d($data);
-d('email');
-die();
-
-
 $urlArgs = _cg("url_vars");
 
-d($_REQUEST);
-d($_REQUEST['email']);
-d($_REQUEST['password']);
 
 
-$data = q("select * from registration where email =$_REQUEST'[email]' ");
-d($data);
-die();
 
-if (isset($_REQUEST['sbt_btn'])) {
+
+
+
+
+//d($_REQUEST);
+//d($_REQUEST['email']);
+//d($_REQUEST['password']);
+
+
+              $email= _escape($_REQUEST['email']);
+              $password = _escape($_REQUEST['password']);
+              
+             $data = q("select email,password from registration where email = '$email'  AND password = '$password' ");
+         //   d($data);
+
+             
+             
+if ($data==1)
+    {
  
-    if ($_REQUEST['email']) {
-
-        $email = _escape($_REQUEST['email']);
-        $password = _escape($_REQUEST['password']);
-        if (Registration::doRegistration($email, $password)) {
-            Registration::initEmailSession($email);
-            //_R(lr('home'));
-            _R(lr('my_account'));
-        } else {
+          _R(lr('my_account'));
+        } 
+        
+        else {
+        
             $error = "Invalid Credentials";
-        }
-        $jsInclude = "home.js.php";
-    }
-    $no_visible_elements = true;
-} else {
-  _R(lr('my_account'));
-
+        
+            // _R(lr('my_account'));
 }
 _cg("page_title", "my_account");
 ?>
